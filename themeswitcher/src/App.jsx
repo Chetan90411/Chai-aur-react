e2c6f7f55547bf21113257bc9f1ca6f6027d1edc
Chetan90411 <chetan738090@gmail.com>
2024-03-29 07:13:41 +0530

    done with context and localstorage

--- themeswitcher/src/App.jsx
+++ themeswitcher/src/App.jsx
@@ -1,4 +1,4 @@
-import React, { useEffect, useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
 import { ThemeProvider } from "./context/theme";
 import ThemeBtn from "./components/ThemeBtn";
@@ -26,7 +26,9 @@ function App() {
           <div className="w-full max-w-sm mx-auto flex justify-end mb-4">
             <ThemeBtn />
           </div>
-          <div className="w-full max-w-sm mx-auto"><Card /></div>
+          <div className="w-full max-w-sm mx-auto">
+            <Card />
+          </div>
         </div>
       </div>
     </ThemeProvider>
